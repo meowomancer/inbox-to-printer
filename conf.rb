69e0561070cd2d340dbcd5b804c3b82450f298f5
zyamada <zachary@yamada.io>
2015-04-09 04:07:08 +0000

    Annotated config file

--- conf.rb
+++ conf.rb
@@ -1,15 +1,27 @@
+#####
+# Inbox To Printer configuration file. Details regarding configuration can be
+# found at https://github.com/zyamada/inbox-to-printer/wiki/Detailed-Configuration
+#####
+
+# If this value is true, then the program will slowly decrease the amount of
+# delay between checking inboxes until there is no delay between going from
+# one inbox to another. If an error is encoutered the delay is reset to its
+# default value. 
 $speedup = true
+
+# The amount of time between
 $delay = 15 
 
+#Array containing all configued inboxes
 @inboxes = [
 	{
-		:host  	=> "",
-		:port 	=> "",
-		:user 	=> "",
-		:pass 	=> "",
-		:printer => "",
-		:valid_senders => [],
-		:invalid_subjects => []
+		:host  	=> "",		#IP or Hostname of IMAP server
+		:port 	=> "",		#Port of the IMAP server
+		:user 	=> "",		#Username of IMAP account
+		:pass 	=> "",		#Password of IMAP account
+		:printer => "",		#CUPS name of printer to print to
+		:valid_senders => [],	#Array of valid senders (RegEx OK)
+		:invalid_subjects => []	#Array of invalid subject terms (Regex OK)
 	},
 
 	{
